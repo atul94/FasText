49e36ea5b839f395a44c079a4b4da4eb03f00fc0
atul94 <atulhyd2013@outlook.com>
2016-03-18 21:25:10 +0530

    More efficient and debug

--- suppress.m
+++ suppress.m
@@ -1,5 +1,5 @@
 function [SBK,SEK,check_b_d]=suppress(SBK,SEK,check_b_d)
-
+temp=check_b_d;
 [row col] = size(SBK);
 for i = 1 : row-2
     for j = 1 : col-2
@@ -8,59 +8,21 @@ for i = 1 : row-2
         T(7)=check_b_d(i+2,j)*SBK(i+2,j);T(8)=check_b_d(i+2,j+1)*SBK(i+2,j+1);T(9)=check_b_d(i+2,j+2)*SBK(i+2,j+2);
         [Max I]=max(T);
         [Min J]=min(T);
-        a=0;b=0;a1=0;b1=0;
-        if(Max>0)
-            x=I/3;y=mod(I,3);
-            if(x<=1)
-                a=0;
-            elseif(x<=2&&x>1)
-                a=1;
-            else
-                a=2;
-            end
-            if(y==1)
-                b=0;
-            elseif(y==2)
-                b=1;
-            else
-                b=2;
-            end
-        end
-        if(Min<0)
-            x=J/3;y=mod(J,3);
-            if(x<=1)
-                a1=0;
-            elseif(x<=2&&x>1)
-                a1=1;
-            else
-                a1=2;
-            end
-            if(y==1)
-                b1=0;
-            elseif(y==2)
-                b1=1;
-            else
-                b1=2;
-            end
-        end
-        if(Max>0||Min<0)
-            tt=SBK(i+a,j+b);
-            tt2=check_b_d(i+a,j+b);
-            ttt=SBK(i+a1,j+b1);
-            ttt2=check_b_d(i+a1,j+b1);
-            for k = 1 : 3
-                for l = 1 : 3
-                    SBK(i+k-1,j+l-1)=0;
-                    check_b_d(i+k-1,j+l-1)=0;
+        for k = 1 : 3
+            for l = 1 : 3
+                x=i+k-1;y=i+l-1;
+                if(check_b_d(x,y)==1&&SBK(x,y)>0)
+                    if(SBK(x,y)~=Max)
+                        SBK(x,y)=0;
+                        check_b_d(x,y)=0;
+                    end
+                elseif(check_b_d(x,y)==-1&&SBK(x,y)>0)
+                    z=SBK(x,y)+Min;
+                    if(z~=0)
+                        SBK(x,y)=0;
+                        check_b_d(x,y)=0;
+                    end
                 end
-            end
-            if(Max>0)
-                SBK(i+a,j+b)=tt;
-                check_b_d(i+a,j+b)=tt2;
-            end
-            if(Min<0)
-                SBK(i+a1,j+b1)=ttt;
-                check_b_d(i+a1,j+b1)=ttt2;
             end
         end
     end
@@ -72,58 +34,21 @@ for i = 1 : row-2
         T(7)=check_b_d(i+2,j)*SEK(i+2,j);T(8)=check_b_d(i+2,j+1)*SEK(i+2,j+1);T(9)=check_b_d(i+2,j+2)*SEK(i+2,j+2);
         [Max I]=max(T);
         [Min J]=min(T);
-        if(Max>0)
-            x=I/3;y=mod(I,3);
-            if(x<=1)
-                a=0;
-            elseif(x<=2&&x>1)
-                a=1;
-            else
-                a=2;
-            end
-            if(y==1)
-                b=0;
-            elseif(y==2)
-                b=1;
-            else
-                b=2;
-            end
-        end
-        if(Min<0)
-            x=J/3;y=mod(J,3);
-            if(x<=1)
-                a1=0;
-            elseif(x<=2&&x>1)
-                a1=1;
-            else
-                a1=2;
-            end
-            if(y==1)
-                b1=0;
-            elseif(y==2)
-                b1=1;
-            else
-                b1=2;
-            end
-        end
-        if(Max>0||Min<0)
-            tt=SEK(i+a,j+b);
-            tt2=check_b_d(i+a,j+b);
-            ttt=SEK(i+a1,j+b1);
-            ttt2=check_b_d(i+a1,j+b1);
-            for k = 1 : 3
-                for l = 1 : 3
-                    SEK(i+k-1,j+l-1)=0;
-                    check_b_d(i+k-1,j+l-1)=0;
+        for k = 1 : 3
+            for l = 1 : 3
+                x=i+k-1;y=i+l-1;
+                if(check_b_d(x,y)==1&&SEK(x,y)>0)
+                    if(SEK(x,y)~=Max)
+                        SEK(x,y)=0;
+                        check_b_d(x,y)=0;
+                    end
+                elseif(check_b_d(x,y)==-1&&SEK(x,y)>0)
+                    z=SEK(x,y)+Min;
+                    if(z~=0)
+                        SEK(x,y)=0;
+                        check_b_d(x,y)=0;
+                    end
                 end
-            end
-            if(Max>0)
-                SEK(i+a,j+b)=tt;
-                check_b_d(i+a,j+b)=tt2;
-            end
-            if(Min<0)
-                SEK(i+a1,j+b1)=ttt;
-                check_b_d(i+a1,j+b1)=ttt2;
             end
         end
     end
