edbdcdf69edcf0d3f8a9482698bdd26150c2ccef
atul94 <atulhyd2013@outlook.com>
2016-03-18 21:24:39 +0530

    Suppress Function change

--- main.m
+++ main.m
@@ -9,16 +9,19 @@
  %max_b_intensity: Matrix equal to the size of image storing the maximum
  %intensity value of the dark pixels in the keypoint circle
  %m: Threshold Parametere separating dark and bright pixels from paper m=13
-clear all;clc;
+clear all;clc;close all;
 %% Reading all all the images
-myDir = 'C:/Users/Atul Agarwal/Desktop/btp1/papers/1/MRRCTrainImages/';
-myDir2 = 'C:/Users/Atul Agarwal/Desktop/btp1/papers/1/MRRCTestImages/';
-ext_img = '*.JPG';
-[my_img,my_img2]=read_images(myDir,myDir2,ext_img);
+% myDir = 'C:/Users/Atul Agarwal/Desktop/btp1/papers/1/MRRCTrainImages/';
+% myDir2 = 'C:/Users/Atul Agarwal/Desktop/btp1/papers/1/MRRCTestImages/';
+% ext_img = '*.JPG';
+% [my_img,my_img2]=read_images(myDir,myDir2,ext_img);
 
 %% The Project begins
-inputimg=my_img2(1).img;
-inputimg=imresize(inputimg,1/(1.6*1.6));
+%inputimg=my_img(52).img;
+inputimg=imread('C:/Users/Atul Agarwal/Desktop/btp1/papers/1/MRRCTestImages/T044.JPG');
+inputimg=imresize(inputimg,1/(1.6*1.6*1.6*1.6));
+figure,
+imshow(inputimg);
 bwimg=rgb2gray(inputimg);
 %bwimg=imresize(bwimg,1/1.6);
 figure,
@@ -27,13 +30,12 @@ imshow(bwimg);
 %% Finding pixels which are either SEK(Stroke End Keypoint) or SBK(Stroke Bend Key Point)
  %Eleminating all pixels with Pi=s and Pi-1!=s
  %Eleminating all pixels with Pi=s and opp(Pi)==s
-SEK=zeros(rows,col);
-SBK=zeros(rows,col);
 check_b_d=zeros(rows,col);%Whether the points surrounding the given point is dark=1 bright=-1 or normal=0
 min_b_intensity=zeros(rows,col);
 max_d_intensity=zeros(rows,col);
 m=13;
-[SEK,SBK,check_b_d,max_d_intensity,min_b_intensity]=find_SEK_SBK(bwimg,SEK,SBK,check_b_d,max_d_intensity,min_b_intensity);
+[SEK,SBK,check_b_d,max_d_intensity,min_b_intensity]=find_SEK_SBK(bwimg,check_b_d,max_d_intensity,min_b_intensity,rows,col);
+
 %% Testing the SEK and SBK thus found
 [row2 col2] = find(SBK>0);
 [row1 col1] = find(SEK>0);
@@ -44,6 +46,7 @@ scatter(col1,row1,'b')
 hold off;
 %% Minimizing key points in a 3*3 neighbourhood
 [SBK,SEK,check_b_d]=suppress(SBK,SEK,check_b_d);
+
 %% Testing again
 [row2 col2] = find(SBK>0);
 [row1 col1] = find(SEK>0);
@@ -81,5 +84,7 @@ end
  
  figure,
  imshow(showimg2/255);
- %% Classification
- 
+%% Classification
+ binimg = rgb2bin(showimg); %check_ff=1 dark point
+ binimg2 = rgb2bin(showimg2); %check_ff=-1 bright point
+  
